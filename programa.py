24b4876daabfb5049eac93fa9f4170ae4a7501cc
Daniel-Daher <141357925+Daniel-Daher@users.noreply.github.com>
2023-10-26 17:11:39 -0300

    Add files via upload

---
+++ programa.py
@@ -0,0 +1,49 @@
+nome = "Daniel"
+print(nome)
+def soma(x, y):
+    resultado = x + y
+    return resultado
+def simples(cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+    elif cor == 'macaco':
+        return 'sabe muito'
+    else:
+        return 'sabe de nada meu irmão'
+
+def simples2(cor):
+    if cor == 'rodrigo':
+        return 'provedel'
+    elif cor == 'daniel':
+        return 'daher'
+    elif cor == 'eduardo':
+        return 'mangeli'
+    else:
+        return 'nao existe'
+
+
+                                
+    
+numeros = [1, 2, 3, 4, 5]
+print(numeros[0])
+print(numeros[ -1])
+numeros[0] = 10
+print(numeros)
+
+
+
+contador = 0
+while contador < 10:
+    print(contador)
+    contador += 1
+for i in range(10):
+    print(i)
+for item in [1,45,78,'a',[3,5]]:
+    print(item)
+for letra in 'minha string':
+    print(letra)
+    
+
+ 
+
+    
